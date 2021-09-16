025027e38abcfc9cf747d284b08dbfc1f0548177
greatvivek11 <vcoolkaushik@gmail.com>
2021-09-16 22:55:17 +0530

    fixed PWA offline caching bug - SW now updates cache on new content if foundm, and removes old cache.

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,8 +1,7 @@
-var cacheName = 'cache-v1';
+var cacheName = 'cache-v2';
 
 var filesToCache = [
-	'/index.html',
-  '/',
+  '/index.html',
   '/dist/assets/index.ff80710c.js',
   '/dist/assets/index.07b5d4d5.css',
   '/manifest.json',
@@ -16,30 +15,27 @@ self.addEventListener('install', function (event) {
   console.log('Event: Install');
 
   event.waitUntil(
-  	//Open the cache
-  	caches.open(cacheName)
-  		.then(function (cache) {
-  			//Adding the files to cache
-  			return cache.addAll(filesToCache)
-  				.then(function () {
-  					console.log("All files are cached.");
-            return self.skipWaiting(); //To forces the waiting service worker to become the active service worker
-  				})
-  		})
-  		.catch(function (err) {
-  			console.log(err)
-  		})
-	);
+    //Open the cache
+    caches.open(cacheName)
+      .then(async function (cache) {
+        //Adding the files to cache
+        await cache.addAll(filesToCache);
+        console.log("All files are cached.");
+      })
+      .catch(function (err) {
+        console.log(err)
+      })
+  );
 });
 
 //Adding 'activate' event listener
 self.addEventListener('activate', function (event) {
   console.log('Event: Activate');
 
-  event.waitUntil( 
-    caches.keys().then(function(cacheNames) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
       return Promise.all(
-        cacheNames.map(function(cache) {
+        cacheNames.map(function (cache) {
           if (cache !== cacheName) {     //cacheName = 'cache-v1'
             return caches.delete(cache); //Deleting the cache
           }
@@ -48,30 +44,30 @@ self.addEventListener('activate', function (event) {
     })
   );
 
-  return self.clients.claim(); //To activate this SW immediately without waiting.
+  // return self.clients.claim(); //To activate this SW immediately without waiting.
 });
 
 //Adding 'fetch' event listener
 self.addEventListener('fetch', function (event) {
+  console.log('WORKER: Fetching');
   var request = event.request;
-  
+
   //Tell the browser to wait for network request and respond with below
   event.respondWith(
     //If request is already in cache, return it
-    caches.match(request).then(function(response) {
+    caches.match(request).then(function (response) {
       if (response) {
         return response;
       }
 
       //else add the request to cache and return the response
-      return fetch(request).then(function(response) {
+      return fetch(request).then(function (response) {
         var responseToCache = response.clone(); //Cloning the response stream in order to add it to cache
         caches.open(cacheName).then(
-          function(cache) {
+          function (cache) {
             cache.put(request, responseToCache); //Adding to cache
           });
         return response;
       });
-    })
-  );
+    }))
 });
