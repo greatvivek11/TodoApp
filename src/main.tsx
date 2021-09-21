a76928777fec0ac7a982ec86e2c4d0f74994c71a
greatvivek11 <vcoolkaushik@gmail.com>
2021-09-21 16:00:36 +0530

    Removed Recoil-nexus from main.

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,10 @@ import { RecoilRoot } from 'recoil';
 import { App } from './app';
 import InitializingServiceWorker from './Components/ServiceWorker';
 import './index.css';
-import RecoilNexus from "recoil-nexus";
 
 InitializingServiceWorker();
 render(
     <RecoilRoot>
-        <RecoilNexus />
         <App />
     </RecoilRoot>
     , document.getElementById('app')!
