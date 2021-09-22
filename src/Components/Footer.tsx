a8bd3a4f4ff3ae53d71a65e1fb45d1e0610fb4ac
greatvivek11 <vcoolkaushik@gmail.com>
2021-09-22 12:32:57 +0530

    Fixed app version in footer from package.json

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,11 +1,11 @@
 import { h } from 'preact';
-import { Version } from "../../version";
 export default function Footer() {
+    const version = import.meta.env.VITE_PACKAGE_VERSION;
     return (
         <footer>
             <div class="sticky inset-x-0 bottom-0 mx-2">
                 <span class="text-sm md:text-base">Made by <a className="text-blue-200" href="https://www.vivekkaushik.in" target="_blank" rel="noopener noreferrer">Vivek Kaushik</a> with ❤️ using </span><span className="text-sm md:text-base font-semibold"> Preact, TailwindCSS, Vite, TS.</span>
-                <p class="text-sm font-bold md:text-base">App Version: {Version} | <a className="text-blue-200" href="https://github.com/greatvivek11/TodoApp" target="_blank" rel="noopener noreferrer">Github</a></p>
+                <p class="text-sm font-bold md:text-base">App Version: {version} | <a className="text-blue-200" href="https://github.com/greatvivek11/TodoApp" target="_blank" rel="noopener noreferrer">Github</a></p>
             </div>
         </footer>
     )
