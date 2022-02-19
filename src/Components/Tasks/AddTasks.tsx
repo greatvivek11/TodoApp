f8b86e4b650169adadec1847ea1997ebfb03bc7e
Vivek Kaushik <54403368+greatvivek11@users.noreply.github.com>
2022-02-19 14:49:18 +0530

    Updated max input length to 100.

--- src/Components/Tasks/AddTasks.tsx
+++ src/Components/Tasks/AddTasks.tsx
@@ -24,7 +24,7 @@ export default function AddTasks() {
   return (
     <div class="container mx-auto mt-2">
       <form class="my-4" autocomplete="off" onSubmit={onSubmit}>
-        <input required minLength={1} maxLength={32} type="text" name="addTask" class="mx-2 rounded-full text-black text-center border border-transparent focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent" id="addTask" placeholder="Add a task" onInput={(e: any) => addInputChange(e.target.value)} />
+        <input required minLength={1} maxLength={100} type="text" name="addTask" class="mx-2 rounded-full text-black text-center border border-transparent focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent" id="addTask" placeholder="Add a task" onInput={(e: any) => addInputChange(e.target.value)} />
         <button class="border border-red-800 bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 mx-2 mt-4 rounded-lg px-4" type="submit">Add Task</button>
       </form>
     </div>
